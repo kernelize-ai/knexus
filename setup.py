fdb4cd02f5158d78fbf374d1f75937476c4b212b
Simon Waters <simon@kernelize.ai>
2026-06-29 09:57:08 -0500

    [Release] Support more versions of python (#147)

--- setup.py
+++ setup.py
@@ -287,7 +287,7 @@ CLASSIFIERS = BASE_CLASSIFIERS + PYTHON_CLASSIFIERS
 
 setup(
     name=os.environ.get("KNEXUS_WHEEL_NAME", "knexus"),
-    version="0.2.1",
+    version="0.2.2",
     author="Simon Waters, Matthew Leon, Alex Baden",
     author_email="simon@kernelize.ai",
     description="",
